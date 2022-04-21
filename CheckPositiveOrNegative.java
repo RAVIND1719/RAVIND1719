fc89fd1c9eb8bb29b6ad0f622b9f03f016ae5383
RAVIND1719 <99536592+RAVIND1719@users.noreply.github.com>
2022-04-21 18:29:32 +0530

    Create CheckPositiveOrNegative.java

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,17 @@
+package assignments_w1_d1;
+
+public class CheckPositiveOrNegative {
+
+	public static void main(String[] args) {
+		int num = 1;
+		if(num >0) {
+			System.out.println("The given number is positive");
+		
+		} else {
+			System.out.println("The given number is negative");
+			
+		}
+
+	}
+
+}
