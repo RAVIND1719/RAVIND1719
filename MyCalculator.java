d72ce2a69be8cbaf61d70ba2707a9a1daf5ddee1
RAVIND1719 <99536592+RAVIND1719@users.noreply.github.com>
2022-04-22 22:46:43 +0530

    Create MyCalculator.java

---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+package assingments_w1_d2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator myObj = new Calculator();
+		System.out.println(myObj.add(125,135));
+		System.out.println(myObj.subtraction(125.8, 120));
+		System.out.println(myObj.multiplication(35, 45));
+		System.out.println(myObj.Division(200, 4));
+		System.out.println(myObj.modulus(2135, 7));
+		
+	}
+
+}
